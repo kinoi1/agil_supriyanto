76ea94546af4f77a1e178653077536fe68159d9c
kinoi1 <agilsupriyanto14@gmail.com>
2025-10-11 13:21:21 +0700

    add setting next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: 'dist',
+  output: 'export', // ini otomatis menggantikan `next export`
+  images: {
+    unoptimized: true, // GitHub Pages tidak mendukung image optimizer bawaan Next.js
+  },
+  basePath: '/agil_supriyanto', // ganti dengan nama repository kamu
+  assetPrefix: '/agil_supriyanto/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
